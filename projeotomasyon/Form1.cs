eb1445d67f9fbe02c6137d2cdd520a4ab6fcf6da
melikeay45 <75757682+melikeay45@users.noreply.github.com>
2020-12-12 01:55:22 +0300

    Update Form1.cs

--- projeotomasyon/Form1.cs
+++ projeotomasyon/Form1.cs
@@ -20,6 +20,7 @@ namespace projeotomasyon
         private void Form1_Load(object sender, EventArgs e)
         {
             MessageBox.Show("Gitten merhaba");
+            MessageBox.Show("Githubdan merhaba");
         }
     }
 }
